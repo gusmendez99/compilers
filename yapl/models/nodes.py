c9356b99c65f437ab31743359c9670d2b73ed0f0
Gus Mendez <gus.mendez.99@gmail.com>
2022-08-03 20:08:57 -0600

    [REF] nodes and type nodes

--- yapl/models/nodes.py
+++ yapl/models/nodes.py
@@ -1,23 +1,35 @@
-class BaseNode:
-    def __init__(self, context, symbol):
-        self.context = context
-        self.symbol = symbol
+class Context:
+    def __init__(self, parent=None):
+        self.parent = parent
+        self.functions = {}
+        self.variables = set()
+        self.children = []
 
-    @abstractmethod
-    def visit(self, visitor):
-        pass
+    def is_defined(self, identifier: str, args: int = None):
+        if args is not None:
+            if identifier in self.functions:
+                for i in self.functions[identifier]:
+                    if len(i) == args:
+                        return True
+            return self.parent and identifier in self.parent.is_defined(identifier, args)
+        else:
+            return identifier in self.variables or \
+                   (self.parent and self.parent.is_defined(identifier))
 
+    def define(self, identifier: str, args: list = None):
+        if args is not None:
+            if identifier in self.functions.keys() and args not in self.functions[identifier]:
+                self.functions[identifier].append(args)
+                return True
+            elif identifier not in self.functions.keys():
+                self.functions[identifier] = [args]
+                return True
+            return False
+        else:
+            self.variables.add(identifier)
+            return True
 
-# Pure grammar nodes
-class ProgramNode(BaseNode):
-    def __init__(self, context, classes):
-        super().__init__(context, "program")
-        self.classes = classes
-
-    def visit(self, visitor):
-        return visitor.visit(self)
-
-
-class ExpressionNode(BaseNode):
-    def __init__(self, context, symbol):
-        super().__init__(context, symbol)
+    def create_child_context(self):
+        new_context = Context(self)
+        self.children.append(new_context)
+        return new_context
